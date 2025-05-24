74abae8381a0d4baef4ae480abdd6464b5b095d8
Anderson Reis <anderson36.reis86@gmail.com>
2025-05-24 15:14:07 -0300

    Adicionando timeout

--- src/main/java/br/com/mentoria/automacao/web/config/BaseTest.java
+++ src/main/java/br/com/mentoria/automacao/web/config/BaseTest.java
@@ -3,12 +3,15 @@ package br.com.mentoria.automacao.web.config;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.concurrent.TimeUnit;
+
 public class BaseTest {
 
     public WebDriver driver = webDriver();
     private WebDriver webDriver(){
         WebDriver webDriver = new ChromeDriver();
         webDriver.manage().window().maximize();
+        webDriver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
 
         return webDriver;
     }
